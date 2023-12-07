6470e460fda4b961b6b97913e315205103312d8f
Jyoti-Nigam <jyoti28.nigam@gmail.com>
2023-12-07 14:28:16 +0530

    some changes

--- Controllers/billingController.js
+++ Controllers/billingController.js
@@ -1,8 +1,36 @@
-// backend/controllers/billingController.js
+// // backend/controllers/billingController.js
+// const Bill = require('../Models/Bill');
+
+// const createBill = async (req, res) => {
+//   const { customerName, totalAmount, items } = req.body;
+
+//   try {
+//     const newBill = new Bill({
+//       customerName,
+//       totalAmount,
+//       items,
+//     });
+
+//     await newBill.save();
+//     res.status(201).json(newBill);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
+// module.exports = {
+//   createBill,
+// };
+
 const Bill = require('../Models/Bill');
 
 const createBill = async (req, res) => {
   const { customerName, totalAmount, items } = req.body;
+
+  // Add validation for required fields
+  if (!customerName || !totalAmount || !items) {
+    return res.status(400).json({ error: 'All fields are required.' });
+  }
 
   try {
     const newBill = new Bill({
@@ -11,14 +39,22 @@ const createBill = async (req, res) => {
       items,
     });
 
+    // Save the new bill
     await newBill.save();
+
     res.status(201).json(newBill);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error.message);
+
+    // Handle specific MongoDB duplicate key error
+    if (error.code === 11000) {
+      return res.status(400).json({ error: 'Bill with the same data already exists.' });
+    }
+
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
 module.exports = {
   createBill,
 };
-
